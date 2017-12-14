5ad15af83bdc25c476891217091c4677be4d795c
Penguizilla <maxjraabe@gmail.com>
2017-12-14 13:49:22 -0500

    Added Backseat Driver

---
+++ Backseat_Driver/offline.js
@@ -0,0 +1,1 @@
+{"version":1513277029215,"fileList":["c2runtime.js","data.js","start.js","style.css","offlineClient.js","images/shared-1-sheet1.png","images/shared-0-sheet0.png","images/shared-1-sheet0.png","icons/icon-16.png","icons/loading-logo.png","icons/icon-32.png","icons/icon-128.png","icons/icon-64.png","icons/icon-256.png","icons/icon-512.png","register-sw.js"]}
